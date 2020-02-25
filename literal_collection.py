9811ea5033e6a769a0cac630c057bc8a6316b498
Preethibharathi28 <60912772+Preethibharathi28@users.noreply.github.com>
2020-02-25 10:43:48 +0600

    Add files via upload

---
+++ literal_collection.py
@@ -0,0 +1,8 @@
+fruits=["Mango","Orange","Grapes"]      #list
+number=(1,2,3)          #tuple
+vegitable={'c':'carrot','o':'onion'}        #dict
+vowels={'a','e','i','o','u'}        #set
+print(fruits)
+print(number)
+print(vegitable)
+print(vowels)
